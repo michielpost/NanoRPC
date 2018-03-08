9a2489697dc84182439b18d8120221402f0067fd
Michiel Post <michiel@q42.nl>
2018-03-08 16:05:30 +0100

    Check blocks tests

---
+++ NanoRPC.Tests/BlocksTests.cs
@@ -0,0 +1,35 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Threading.Tasks;
+
+namespace NanoRPC.Tests
+{
+  [TestClass]
+  public class BlocksTests
+  {
+    private INanoRPC _client;
+
+    public BlocksTests()
+    {
+      _client = NanoClient.GetClient(Configuration.BaseUrl);
+    }
+
+    [TestMethod]
+    public async Task BlockCount()
+    {
+      var result = await _client.BlockCount(new BlockCountRequest());
+
+      Assert.IsNotNull(result);
+    }
+
+    [TestMethod]
+    public async Task BlockCountType()
+    {
+      var result = await _client.BlockCountType(new BlockCountTypeRequest());
+
+      Assert.IsNotNull(result);
+    }
+
+  }
+}
