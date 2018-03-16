55e054074c03ca16053d9401d8d0ce016ad93059
Michiel Post <michiel@q42.nl>
2018-03-16 22:32:21 +0100

    Updated Frontiers RPC

--- NanoRPC.Tests/FrontiersTests.cs
+++ NanoRPC.Tests/FrontiersTests.cs
@@ -18,13 +18,20 @@ namespace NanoRPC.Tests
     }
 
     [TestMethod]
-    public async Task KeyCreate()
+    public async Task Frontiers()
     {
-      var result = await _client.Frontiers(new FrontiersRequest() { Account = _account, Count = "10" });
+      var result = await _client.Frontiers(new FrontiersRequest() { Account = "xrb_1114g5pesiopr4r5jsdhzgiuc19ed9o881b3d5714w4pd49g3iohdp59p97d", Count = 10 });
 
       Assert.IsNotNull(result);
       Assert.IsNotNull(result.Frontiers);
     }
 
+    [TestMethod]
+    public async Task FrontierCount()
+    {
+      var result = await _client.FrontierCount(new FrontierCountRequest());
+
+      Assert.IsNotNull(result);
+    }
   }
 }
